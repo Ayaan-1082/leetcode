569ec82686d73faaf70c283a28f869c52286119c
Ayaan-1082 <khanayaan1800@gmail.com>
2026-07-13 17:09:46 +0530

    Time: 27 ms (24.13%), Space: 13.5 MB (19.12%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,23 +1,24 @@
 class Solution(object):
     def isAnagram(self, s, t):
-        if len(s) != len(t) :
-            return False
-        d1 = {}
-        for a in s:
-            if a not in d1.keys():
-                d1[a] = 1
-            else :
-                d1[a] = d1[a] + 1
-        for a in t:
-            if a not in d1.keys():
-                return False
-            else :
-                d1[a] = d1[a] - 1
-        for k in d1.values():
-            if k!=0:
-                return False
+        # if len(s) != len(t) :
+        #     return False
+        # d1 = {}
+        # for a in s:
+        #     if a not in d1.keys():
+        #         d1[a] = 1
+        #     else :
+        #         d1[a] = d1[a] + 1
+        # for a in t:
+        #     if a not in d1.keys():
+        #         return False
+        #     else :
+        #         d1[a] = d1[a] - 1
+        # for k in d1.values():
+        #     if k!=0:
+        #         return False
 
-        return True
+        # return True
+        return sorted(s)==sorted(t)
         """
         :type s: str
         :type t: str
